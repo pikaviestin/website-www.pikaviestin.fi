67438347e4dd063b0b92d30767342129bb6105f8
Aminda Suomalainen <suomalainen+git@mikaela.info>
2024-07-08 06:37:25 +0300

    install chrome through puppeteer for md-to-pdf

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 printf "Ensuring dependencies are installed...\n"
 corepack npm install
+corepack npm exec @puppeteer/browsers install chrome@stable
 bundle install
 printf "Building tietosuojaseloste...\n"
 corepack npm exec md-to-pdf -- tietosuojaseloste.md
