e8e45086c55d23bb0d6252db11fd0962370526be
Aminda Suomalainen <suomalainen+git@mikaela.info>
2024-07-08 06:50:34 +0300

    build.sh: use npx for md-to-pdf after all

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 printf "Ensuring dependencies are installed...\n"
-corepack npm install
-corepack npm exec @puppeteer/browsers install chrome@stable
 bundle install
 printf "Building tietosuojaseloste...\n"
-corepack npm exec md-to-pdf -- tietosuojaseloste.md
+corepack npx md-to-pdf -- tietosuojaseloste.md
 printf "Building website...\n"
 bundle exec jekyll build
