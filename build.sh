5b7b06ac1268f5e3d9c1aba7605c4751e6e232dc
Aminda Suomalainen <suomalainen+git@mikaela.info>
2024-07-08 06:28:36 +0300

    build.sh: explicitly run npm install & bundle install

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+printf "Ensuring dependencies are installed...\n"
+corepack npm install
+bundle install
 printf "Building tietosuojaseloste...\n"
 corepack npm exec md-to-pdf -- tietosuojaseloste.md
 printf "Building website...\n"
