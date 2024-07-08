4b1bfd4ea0b502e4b5857ba9747a4c3321edfff7
Aminda Suomalainen <suomalainen+git@mikaela.info>
2024-07-08 06:53:13 +0300

    build.sh: add the puppeteer command

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 printf "Ensuring dependencies are installed...\n"
 bundle install
+corepack npx puppeteer browsers install chrome
 printf "Building tietosuojaseloste...\n"
 corepack npx md-to-pdf -- tietosuojaseloste.md
 printf "Building website...\n"
