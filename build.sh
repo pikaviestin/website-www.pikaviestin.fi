9592a9eabfcb487fa2a928d6ee91b928c6c2a088
Aminda Suomalainen ⚧ <suomalainen+git@mikaela.info>
2024-07-09 14:51:06 +0300

    Small fixes that were bothering me (#18)
    * _config.yml: include CNAME and tietosuojaseloste.pdf directly rather than having to cp them in workflow
    * update ruby & npm
    * add .gitattributes
    * Normalize line-endings
    * add a simple .github/renovate.json5
    * Simplify generation of tietosuojaseloste.pdf
    * .github/renovate.json5: explicitly disable automerge
    * main workflow: use lts node instead of current
    * generate pdf for all builds
    * main.yaml: update setup-ruby, remove extraneous ruby-version line
    * possibly cache md-to-pdf
    * fix build and node setup for main workflow
    * include package-lock.json due to being required by the setup node workflow
    * clean build.sh a bit
    * build.sh: explicitly run npm install & bundle install
    * install chrome through puppeteer for md-to-pdf
    * build.sh: use npx for md-to-pdf after all
    * build.sh: add the puppeteer command
    * main workflow: don't pin specific setup-ruby commit as per their README

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+printf "Ensuring dependencies are installed...\n"
+bundle install
+corepack npx puppeteer browsers install chrome
+printf "Building tietosuojaseloste...\n"
+corepack npx md-to-pdf -- tietosuojaseloste.md
+printf "Building website...\n"
+bundle exec jekyll build
