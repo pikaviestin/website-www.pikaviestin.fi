883d50c5b461d7559d24f608b01dd329fa0d1dec
Skyler Mäntysaari <sm+git@skym.fi>
2025-02-05 07:17:11 +0200

    Damn it

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 printf "Ensuring dependencies are installed...\n"
 bundle install
-corepack prepare pnpm@10.0.0 --activate # Related bug: https://github.com/nodejs/corepack/issues/612
+npm install -g corepack@0.31.0 # Related bug: https://github.com/nodejs/corepack/issues/612
 corepack npx puppeteer browsers install chrome
 printf "Building tietosuojaseloste...\n"
 corepack npx md-to-pdf -- tietosuojaseloste.md
