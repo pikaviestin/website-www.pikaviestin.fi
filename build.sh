742386f99ec6c6130542c64c5cb0c9a55df485ce
Skyler Mäntysaari <sm+git@skym.fi>
2025-02-05 07:14:52 +0200

    Fix the build problems with corepack.
    Related to https://github.com/nodejs/corepack/issues/612

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 printf "Ensuring dependencies are installed...\n"
 bundle install
+corepack prepare pnpm@10.0.0 --activate # Related bug: https://github.com/nodejs/corepack/issues/612
 corepack npx puppeteer browsers install chrome
 printf "Building tietosuojaseloste...\n"
 corepack npx md-to-pdf -- tietosuojaseloste.md
